ae996f029c5e4170d71b74a943975cca80aeae77
cdJohnEl <easykelchimdikejohn@gmail.com>
2023-12-23 13:19:49 +0100

    Done with all Tasks

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""A script that starts a flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+/hbnb displays HBNB
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Return a given string"""
+    return ("Hello HBNB!")
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Return a given string"""
+    return ("HBNB")
+
+@app.route('/c<text>', strict_slashes=False)
+def cText(text):
+    """Return C followed by the value of text variable"""
+    return "C {}".format(text.replace("_", " "))
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythonText(text="is cool"):
+    """Return python followed by the value of text variable"""
+    return "Python {}".format(text.replace("_", " "))
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
